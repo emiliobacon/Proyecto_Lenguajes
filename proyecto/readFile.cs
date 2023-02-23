cb8a6125b237ffd2b9796c05328f77ea2eea4dea
emiliobacon <eabarillasco@correo.url.edu.gt>
2023-02-23 12:12:43 -0600

    program reads files and prints line by line

---
+++ proyecto/readFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace proyecto
+{
+	public class readFile
+	{
+        public static void Read()
+        {
+            string filePath = "/Users/emilio/Desktop/proyecto/proyecto/docs/GRAMATICA.txt";
+
+            // Abre el archivo utilizando StreamReader
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                // Lee el archivo línea por línea
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Procesa la línea leída del archivo
+                    Console.WriteLine(line);
+                }
+            } 
+
+            // Cierra el archivo
+            Console.ReadLine();
+        }
+    }
+}
+

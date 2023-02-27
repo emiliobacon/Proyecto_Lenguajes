501521ccaaa2bd7b825789c9126646e1cf06d3be
emiliobacon <eabarillasco@correo.url.edu.gt>
2023-02-26 23:36:51 -0600

    update

--- proyecto/readFile.cs
+++ proyecto/readFile.cs
@@ -7,31 +7,7 @@ namespace proyecto
 	public class readFile
 	{
         
-        public static void Read()
-        {
-            int contador = 0;
-            int i = 0;
-            List<string> txt = new List<string>();
-            string filePath = "/Users/emilio/Desktop/proyecto/proyecto/docs/GRAMATICA.txt";
 
-            // Abre el archivo utilizando StreamReader
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                // Lee el archivo línea por línea
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    // Guardar cada línea en una lista para su uso posterior
-                    txt.Add(line);
-                    contador++;
-                }
-            }
-
-
-           
-            
-     
-        }
 
         
     }

e1bce60151a8b119e026490f566f729a5906c722
Megan Morales <63917946+Megan-Morales@users.noreply.github.com>
2023-03-28 21:12:52 -0600

    actualización

--- proyecto/readFile.cs
+++ proyecto/readFile.cs
@@ -13,11 +13,11 @@ namespace proyecto
             int contadorLineas = 0;
             List<string> txt = new List<string>();
 
-            //string filePath = "C:\\Users\\megan\\OneDrive\\Escritorio\\Megan\\proyectos_oficial\\C#\\Proyecto_Lenguajes\\proyecto\\docs\\GRAMATICA.txt";
+            string filePath = "C:\\Users\\megan\\OneDrive\\Escritorio\\Megan\\proyectos_oficial\\C#\\Proyecto_Lenguajes\\proyecto\\docs\\GRAMATICA.txt";
 
 
             //string filePath = "/Users/emilio/Desktop/proyecto/proyecto/docs/GRAMATICA.txt";
-           string filePath = "C:\\Users\\Roberto Moya\\Desktop\\ProyectoAutomatas\\proyecto\\docs\\GRAMATICA.txt";
+            //string filePath = "C:\\Users\\Roberto Moya\\Desktop\\ProyectoAutomatas\\proyecto\\docs\\GRAMATICA.txt";
 
             int a = 0;
             // Abre el archivo utilizando StreamReader

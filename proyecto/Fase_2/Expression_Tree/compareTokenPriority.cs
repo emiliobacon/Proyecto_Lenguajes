26602777b6cce3906276c8b129d829844c29f150
emilio <eabarillasco@correo.url.edu.gt>
2023-04-02 12:43:26 -0600

    update

--- proyecto/Fase_2/Expression_Tree/compareTokenPriority.cs
+++ proyecto/Fase_2/Expression_Tree/compareTokenPriority.cs
@@ -1,38 +1,23 @@
 ﻿namespace expression_tree
 {
-    public abstract class CompareTokenPriority
+    public static class CompareTokenPriority
     {
-        private static Dictionary<string, int>? _priorityMap;
+        private static readonly Dictionary<string, int> PriorityMap = new Dictionary<string, int>
+        {
+            ["*"] = 2,
+            ["+"] = 2,
+            ["?"] = 2,
+            ["."] = 3,
+            ["|"] = 4,
+            ["("] = 1
+        };
 
         public static int CompararPrioridad(string token, string comp)
         {
-            // Inicializa el diccionario con los símbolos y sus prioridades
-            _priorityMap = new Dictionary<string, int>();
-            
-            _priorityMap["*"] = 2;
-            _priorityMap["+"] = 2;
-            _priorityMap["?"] = 2;
-            _priorityMap["."] = 3;
-            _priorityMap["|"] = 4;
-            _priorityMap["("] = 1;
+            var priority1 = PriorityMap[token];
+            var priority2 = PriorityMap[comp];
 
-            var priority1 = _priorityMap[token];
-            var priority2 = _priorityMap[comp];
-            
-            
-            if (priority1 < priority2)
-            {
-                return 1;
-            }
-            else if (priority1 > priority2)
-            {
-                return 0;
-            }
-            else
-            {
-                // Si los dos símbolos tienen la misma prioridad, devuelve 0
-                return 0;
-            }
+            return priority1.CompareTo(priority2);
         }
     }
 }
